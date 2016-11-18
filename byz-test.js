d052afcccaa9eab9a6a9cdf9cfc8eb027e277f8a
Biaoyinzi <phoneticchar@gmail.com>
2016-11-18 00:18:50 -0800

    Add byz-test.js

---
+++ byz-test.js
@@ -0,0 +1,17 @@
+const byz = require('./byz.js');
+
+function main() {
+  console.log("Mandarin");
+  console.log(byz.getByz("zhong"));
+  console.log(byz.getByz("guo2"));
+
+  console.log("Cantonese");
+  console.log(byz.getByz("zung7")); // Jyutpin: zung3
+  console.log(byz.getByz("guok7")); // Jyutpin: gwok3
+
+  console.log("Korean");
+  console.log(byz.getByz("zhung"));
+  console.log(byz.getByz("gug"));
+}
+
+main();
